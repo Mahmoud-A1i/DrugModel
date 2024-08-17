ca96ee29dd4984420ffcf4d8803905aae39df49d
Mahmoud-A1i <158025114+Mahmoud-A1i@users.noreply.github.com>
2024-08-17 17:53:55 +0300

    Update __init__.py
    Updated to include libraries from sascorer.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,13 @@
 from .loss import SMILESLoss
 from .metrics import partial_smiles, get_mol_props, calculate_similarity, test
+from .sascorer import calculateScore, fraction_valid, novelty
 
 __all__ = [
     'SMILESLoss',
     'get_mol_props',
     'calculate_similarity',
-    'test'
+    'test',
+    'calculateScore',
+    'fraction_valid',
+    'novelty'
 ]
